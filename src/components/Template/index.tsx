73c641f3f0feb354030f1bc6dca220dc96caf83d
Renan Veronez Drechsler <renanvz@outlook.com>
2022-10-17 11:28:33 +0100

    Header

---
+++ src/components/Template/index.tsx
@@ -0,0 +1,7 @@
+export function Template() {
+  return (
+    <div>
+
+    </div>
+  )
+}
---
+++ src/components/Template/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+background: #fff;
+padding: 0 0 0 0;
+`
